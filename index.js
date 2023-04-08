08f80d4dbabb52f1cee175c6a5bf815aa9836fe3
samuel-durand <samuel.durand@laplateforme.io>
2023-04-08 11:49:55 +0200

    clock work

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ function updateClock(){
     var currentTime= new Date();
 
     //obtenir les heure ,minute et secondes
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var currentSeconds = currentTime.getSeconds();
+
+    // ajouter un zéro devant les minutes et les seconde si elles sont inferieur à 10
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    // mettre à jour l'horloge 
+
+    document.getElementById("hours").innerHTML = currentHours;
+    document.getElementById("minutes").innerHTML = currentMinutes;
+    document.getElementById("seconds").innerHTML = currentSeconds;
 }
+
+setInterval(updateClock,1000)
