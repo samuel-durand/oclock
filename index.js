9dca8c5b929dd4d5f4620530cccf68d9de8b7225
samuel-durand <samuel.durand@laplateforme.io>
2023-04-08 12:26:41 +0200

    need to work in timer

--- index.js
+++ index.js
@@ -19,3 +19,70 @@ function updateClock(){
 }
 
 setInterval(updateClock,1000)
+
+
+var timerInterval; // variable pour stocker l'intervalle du minuter
+
+function startTimer() {
+    // demarrer l'intervar du minuteur
+    timerInterval = setInterval(updateTimer,1000);
+
+}
+
+function stopTimer(){
+    //arreter l'intervalle du timer
+    clearInterval(timerInterval);
+}
+
+function resetTimer(){
+    //réinitialiser le timer
+    stopTimer();
+    document.getElementById("hours").innerHTML ="00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+
+}
+
+
+function updateTimer() {
+    // Obtenir les heures, minutes et secondes restantes
+    var remainingHours = document.getElementById("hours").innerHTML;
+    var remainingMinutes = document.getElementById("minutes").innerHTML;
+    var remainingSeconds = document.getElementById("seconds").innerHTML;
+  
+    // Soustraire une seconde
+    if (remainingSeconds == "00") {
+      if (remainingMinutes == "00") {
+        if (remainingHours == "00") {
+          // Le minuteur est terminé
+          stopTimer();
+          alert("Le minuteur est terminé!");
+          return;
+        }
+        remainingHours--;
+        remainingMinutes = 59;
+      } else {
+        remainingMinutes--;
+      }
+      remainingSeconds = 59;
+    } else {
+      remainingSeconds--;
+    }
+  
+    // Ajouter un zéro devant les heures, minutes et secondes si elles sont inférieures à 10
+    remainingHours = (remainingHours < 10 ? "0" : "") + remainingHours;
+    remainingMinutes = (remainingMinutes < 10 ? "0" : "") + remainingMinutes;
+    remainingSeconds = (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
+  
+    // Mettre à jour le minuteur
+    document.getElementById("hours").innerHTML = remainingHours;
+    document.getElementById("minutes").innerHTML = remainingMinutes;
+    document.getElementById("seconds").innerHTML = remainingSeconds;
+  }
+
+
+
+// Ajouter des écouteurs d'événements aux boutons pour démarrer, arrêter et réinitialiser le minuteur
+document.getElementById("start").addEventListener("click", startTimer);
+document.getElementById("stop").addEventListener("click", stopTimer);
+document.getElementById("reset").addEventListener("click", resetTimer);
