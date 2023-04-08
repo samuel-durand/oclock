58e41ce66fc598c36213214928bb225fb4a72174
samuel-durand <samuel.durand@laplateforme.io>
2023-04-08 18:17:30 +0200

    update ring and html bug js

--- index.js
+++ index.js
@@ -98,14 +98,13 @@ function zeroPad(num) {
 }
 
 
-//alarme
-
-var alarmInterval = null;
+var alarmIntervals = [];
 var alarmHours = document.getElementById("alarm-hours");
 var alarmMinutes = document.getElementById("alarm-minutes");
 var alarmMessage = document.getElementById("alarm-message");
+var alarmList = document.getElementById("alarm-list");
 
-document.getElementById("set-alarm").addEventListener("click", function() {
+function setAlarm() {
   // Régler l'alarme
   var now = new Date();
   var alarmTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), alarmHours.value, alarmMinutes.value, 0, 0);
@@ -115,20 +114,56 @@ document.getElementById("set-alarm").addEventListener("click", function() {
     alert("L'heure de l'alarme est passée!");
     return;
   }
-  alarmInterval = setTimeout(function() {
+  var alarmInterval = setTimeout(function() {
     // Sonner l'alarme
-    var message = alarmMessage.value || "Réveil!!";
-    alert(message);
+    alert(alarmMessage.value || "Réveil!!");
+    alarmIntervals.splice(alarmIntervals.indexOf(alarmInterval), 1);
+    updateAlarmList();
   }, timeToAlarm);
-});
+  alarmIntervals.push(alarmInterval);
+  updateAlarmList();
+}
 
-document.getElementById("clear-alarm").addEventListener("click", function() {
-  // Effacer l'alarme
-  clearTimeout(alarmInterval);
-  alarmHours.value = 0;
-  alarmMinutes.value = 0;
-  alarmMessage.value = "";
-});
+function clearAlarm() {
+  // Effacer toutes les alarmes
+  for (var i = 0; i < alarmIntervals.length; i++) {
+    clearTimeout(alarmIntervals[i]);
+  }
+  alarmIntervals = [];
+  updateAlarmList();
+}
+
+function updateAlarmList() {
+  // Mettre à jour la liste des alarmes
+  alarmList.innerHTML = "";
+  for (var i = 0; i < alarmIntervals.length; i++) {
+    var timeToAlarm = alarmIntervals[i]._idleTimeout;
+    var message = alarmMessage.value || "Alarme";
+    var alarmTime = new Date(Date.now() + timeToAlarm);
+    var hours = alarmTime.getHours();
+    var minutes = alarmTime.getMinutes();
+    var alarmListItem = document.createElement("li");
+    if (timeToAlarm <= 0) {
+      // L'heure de l'alarme est passée
+      alarmListItem.innerText = message + " passée";
+    } else {
+      // L'heure de l'alarme est future
+      var timeToAlarmInSeconds = Math.round(timeToAlarm / 1000);
+      var minutes = Math.floor(timeToAlarmInSeconds / 60);
+      var seconds = timeToAlarmInSeconds % 60;
+      alarmListItem.innerText = message + " à " + hours + "h" + minutes;
+    }
+    alarmList.appendChild(alarmListItem);
+  }
+}
+
+
+document.getElementById("set-alarm").addEventListener("click", setAlarm);
+document.getElementById("clear-alarm").addEventListener("click", clearAlarm);
+
+
+
+
 
 
 
